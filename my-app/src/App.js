a5019ec4ac547ae090c9bb40063cbc3279bdf23e
ankurvaseashta <root@USD1703230.local>
2018-08-03 16:15:10 -0400

    Added toolsJS to the repo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { hello, user }  from './tools';
 
 class App extends Component {
+
+    state = {
+      open: false,
+    };
+
+    handleClose = () => {
+      this.setState({
+        open: false,
+      });
+    };
+
+    handleClick = () => {
+      this.setState({
+        open: true,
+      });
+    };
+
+
   render() {
+  const { classes } = this.props;
+  const { open } = this.state;
+
     return (
-      <div className="App" >
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React App</h1>
         </header>
         <p className="App-intro">
-          <code>Welcome to my web app!</code>
-          <button class="button" onClick={() => alert(' ¯\\_(ツ)_/¯ ')}> Happy? </ button>
+           <code>{user.firstName}, welcome to my web app!</code>
+
+          <button class="button" onClick={hello}> Hello </ button>
+          <button class="button" onClick={() => alert(' ¯\\_(ツ)_/¯ ')}> test </ button>
         </p>
       </div>
     );
---
+++ my-app/src/tools.js
@@ -0,0 +1,9 @@
+export function hello(){
+     return alert(' ¯\\_(ツ)_/¯ ');
+}
+
+export const user = {  
+  firstName: 'Buddy',
+  lastName: 'Vaseashta',
+  level: 'React 101'
+};
