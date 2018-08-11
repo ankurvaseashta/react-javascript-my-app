20c0db3c10a3bd5ab46b597509681424638b8649
ankurvaseashta <root@USD1703230.local>
2018-08-11 12:35:35 -0400

    new lessons on primitive types and reference types

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,48 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import { hello, user }  from './tools';
 
-/*ES6 refresher*/
-let myName = 'Ankur';
-console.log("Welcome " + myName);
-
-myName = 'AnkurTest';
-console.log(myName);
-
-const printMyName = (name, age) => {
-  console.log(name, age);
-}
-printMyName('Max', 33);
-
-const multiply = number => number * 2;
-console.log(multiply(2));
-/*ES6*/
-
-/*Spread operator*/
-const numbers = [1,2,3];
-const newNumbers = [...numbers, 4];
-
-console.log(newNumbers);
-
-const person = {
-  name: 'Ankur'
-}
-
-const newPerson = {
-  ...person,
-  age: 33
-}
-
-console.log(newPerson);
-/*Spread operator*/
-
-/*Rest operator*/
-const filter = (...args) => {
-  return args.filter(el => el === 1);
-}
-
-console.log(filter(1,2,3));
-/*Rest operator*/
-
 
 function Person(props){
   return (
@@ -80,7 +38,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React App</h1>
+          <h1 className="App-title"> React App</h1>
         </header>
         <p className="App-intro">
            <code>{user.firstName}, welcome to my web app!</code>
--- my-app/src/tools.js
+++ my-app/src/tools.js
@@ -8,7 +8,9 @@ export const user = {
   level: 'React 101'
 };
 
+/********* Lesson ************/
 /*ES6 refresher*/
+/********* Lesson ************/
 let myName = 'Ankur';
 console.log("Welcome " + myName);
 
@@ -24,7 +26,10 @@ const multiply = number => number * 2;
 console.log(multiply(2));
 /*ES6*/
 
+
+/********* Lesson ************/
 /*Spread operator (...) */
+/********* Lesson ************/
 const numbers = [1,2,3];
 const newNumbers = [...numbers, 4];
 
@@ -42,7 +47,9 @@ const newPerson = {
 console.log(newPerson);
 /*Spread operator*/
 
+/********* Lesson ************/
 /*Rest operator*/
+/********* Lesson ************/
 const filter = (...args) => {
   return args.filter(el => el === 1);
 }
@@ -50,7 +57,9 @@ const filter = (...args) => {
 console.log(filter(1,2,3));
 /*Rest operator*/
 
+/********* Lesson ************/
 /*Destructuring Object and Array*/
+/********* Lesson ************/
 /*Arrray Destructuring*/
 let array = ['Hello','Max'];
 console.log(array[1]);
@@ -66,3 +75,36 @@ let {name,age} = {name: 'Ankur', age: 33};
 console.log(name);
 console.log(age);
 /*Arrray Destructuring*/
+
+/********* Lesson ************/
+/*Reference vs Primitive Type*/
+/********* Lesson ************/
+/*Primitive Type*/
+const number = 1;
+
+/*Reference Type*/
+const num2 = number;
+console.log(num2);
+
+
+/*person the object is stored in memory and const person is a pointer to it*/
+const person0 = {
+  name: 'Ankur'
+};
+/*copy of pointer not object (could be an issue)*/
+const secondPerson = person0;
+person0.name = 'Manu';
+console.log(secondPerson);
+
+
+
+/*person the object is stored in memory and const person is a pointer to it*/
+const person1 = {
+  name: 'Ankur1'
+};
+/* use spread to copy the object and not the pointer to the object)*/
+const secondPerson1 = {
+  ...person1
+};
+person.name = 'Manu1';
+console.log(secondPerson1);
